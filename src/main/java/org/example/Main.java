6c8cbe995c500ee7881e6ad85e585d67b2ab3dac
mancelina24 <feyza.turkay@ogr.iu.edu.tr>
2025-03-03 17:09:09 +0300

    test2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,36 +15,43 @@ public class Main {
         if (clock < 0 || clock > 23) {
             return false;
         }
-        return isBarking && (clock < 8 || clock >= 21);
+
+        if (isBarking) {
+            if (clock >= 0 && clock < 8 || clock >= 20 && clock <= 23) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
 
-        return (firstAge >= 13 && firstAge <= 19) ||
-                (secondAge >= 13 && secondAge <= 19) ||
-                (thirdAge >= 13 && thirdAge <= 19);
+        return (firstAge >= 13 && firstAge <= 19) || (secondAge >= 13 && secondAge <= 19) || (thirdAge >= 13 && thirdAge <= 19);
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-        int upperLimit=isSummer? 45 : 35;
-        return temp>=25 && temp <= upperLimit;
+        if(isSummer){
+            return temp >= 25 && temp <=45;
+        }else{
+            return temp >= 25 && temp <=35;
+        }
     }
 
     public static double area(double width, double height) {
 
-        if (width < 0 || height < 0) {
+        if(width < 0 || height < 0){
             return -1;
         }
-        return width * height;
+        return width*height;
     }
 
 
     public static double area(double radius) {
 
-        if(radius < 0)
-        {
+        if(radius < 0){
             return -1;
         }
+
         return radius*radius*Math.PI;
     }
 }
